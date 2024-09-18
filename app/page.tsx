c11bb8bd17aa85d6e83e019ff5aa3794bc613698
Artyom Sukiasyan <artiom.suqiasyan1401@gmail.com>
2024-09-18 23:41:02 +0400

    feat: optimize main page

--- app/page.tsx
+++ app/page.tsx
@@ -5,50 +5,48 @@ import "./global.css";
 import "./page.css";
 
 export default async function Home() {
-  const folders = await getFolders();
+  const folders = getFolderNames();
 
   return (
     <div className="container">
       {folders.map((folder, folderIndex) => (
         <div key={folderIndex}>
-          <h2>{folder.folderName}</h2>
-
+          <h2>{folder}</h2>
           <div className="folder_container">
-            {folder.images.map((image, imageIndex) => (
+            {getImagesByFolder(folder).map((image, imageIndex) => (
               <div className="image_container" key={imageIndex}>
                 <Image
-                  src={`/${folder.folderName}/${image}`}
+                  src={`/${folder}/${image}`}
                   alt={`Image ${imageIndex + 1}`}
                   fill
                 />
               </div>
             ))}
           </div>
+          <div className="folder_container"></div>
         </div>
       ))}
     </div>
   );
 }
 
-export async function getFolders() {
-  const publicDirectory = path.join(process.cwd(), "public");
+const publicDirectory = path.join(process.cwd(), "public");
+
+function getFolderNames() {
   const folderNames = fs
     .readdirSync(publicDirectory)
     .filter((file) =>
       fs.statSync(path.join(publicDirectory, file)).isDirectory()
     );
 
-  const folders = folderNames.map((folderName) => {
-    const folderPath = path.join(publicDirectory, folderName);
-    const images = fs
-      .readdirSync(folderPath)
-      .filter((file) => /\.(png|jpg|jpeg|gif)$/.test(file));
+  return folderNames;
+}
 
-    return {
-      folderName,
-      images,
-    };
-  });
+function getImagesByFolder(folderName: string) {
+  const folderPath = path.join(publicDirectory, folderName);
+  const images = fs
+    .readdirSync(folderPath)
+    .filter((file) => /\.(png|jpg|jpeg|gif)$/.test(file));
 
-  return folders;
+  return images;
 }
